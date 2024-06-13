eff4451d13a8c364d8f4fce3d9664541ad47db08
Daniele-Taullaraj <danieletaullaraj@gmail.com>
2024-06-13 20:18:55 +0200

    BONUS

--- main.js
+++ main.js
@@ -3,18 +3,50 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            coseDaFare: []
+            coseDaFare: [],
+            inputUser: "",
+            contatore: 0,
         }
     },
     methods: {
+        checkStatus(listElement) {
+            if (listElement.done) {
+                return 'text-success fs-5 list-unstyled border border-1 border-success rounded p-2 mb-2';
+            } else {
+                return 'text-decoration-line-through text-danger fs-5 list-unstyled border border-1 border-danger rounded p-2 mb-2';
+            }
+        },
+        countTodo() {
+            this.coseDaFare.forEach(element => {
+                if (element.done) {
+                    this.contatore++;
+                    console.log(this.contatore)
+                }
+            });
+        },
+        addElement() {
+            if (!this.inputUser == "") {
+                this.coseDaFare.push({ "titolo": this.inputUser, "done": true });
+                this.inputUser = "";
+                this.contatore++;
+            }
+        },
+        changeStatus(listElement) {
+            if (listElement.done) {
+                listElement.done = false
+                this.contatore--;
+            } else {
+                listElement.done = true
+                this.contatore++;
+            }
+        }
+
 
     },
     mounted() {
-        console.log("Recupero i dati dal server");
-
         axios.get("api.php").then(results => {
-            console.log("Risultati: ", results);
             this.coseDaFare = results.data;
+            this.countTodo()
         });
     }
 }).mount('#app')
