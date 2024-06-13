4c825a6b54eb2c179efc36dfa289bfe01e6d311b
Daniele-Taullaraj <danieletaullaraj@gmail.com>
2024-06-13 16:37:41 +0200

    stampo in pagina i dati richiamati nello script

---
+++ api.php
@@ -0,0 +1,17 @@
+<?php
+
+$coseDaFare = [
+    [ "titolo" => "Fare la spesa", "done" => false ],
+    [ "titolo" => "Studiare per l'esame", "done" => true ],
+    [ "titolo" => "Andare in palestra", "done" => false ],
+    [ "titolo" => "Pulire la casa", "done" => false ],
+    [ "titolo" => "Leggere un libro", "done" => true ],
+    [ "titolo" => "Scrivere un'email", "done" => false ],
+    [ "titolo" => "Fare una passeggiata", "done" => true ],
+    [ "titolo" => "Guardare un film", "done" => false ]
+];
+
+header('Content-Type=> application/json');
+
+echo json_encode($coseDaFare);
+
